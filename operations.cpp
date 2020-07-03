4d82cc28d5afe9f43af577bf57ca7048cd5383a0
GaboDotCodes <gabedu2000@gmail.com>
2020-07-03 16:30:58 -0500

    Modify c++ code || With 2 functions is easier, sorry :(

--- operations.cpp
+++ operations.cpp
@@ -1,12 +1,20 @@
 #include <node.h>
 using namespace v8;
 
-void operations(const FunctionCallbackInfo<Value>& args) {
+void add(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
 
-  double value = 0;
+  double value = args[0].As<Number>()->Value() + args[1].As<Number>()->Value();
   
-  value = args[1].As<Number>()->Value() + args[2].As<Number>()->Value();
+  Local<Number> num = Number::New(isolate, value);
+
+  args.GetReturnValue().Set(value);
+}
+
+void subtract(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  
+  double value = args[0].As<Number>()->Value() - args[1].As<Number>()->Value();
   
   Local<Number> num = Number::New(isolate, value);
 
@@ -14,7 +22,8 @@ void operations(const FunctionCallbackInfo<Value>& args) {
 }
 
 void Init(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "operations", operations);
+  NODE_SET_METHOD(exports, "add", add);
+  NODE_SET_METHOD(exports, "subtract", subtract);
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
