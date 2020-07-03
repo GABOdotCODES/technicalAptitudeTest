6de6ce7d6b2b9266ba5dec91259170701b440efe
GaboDotCodes <gabedu2000@gmail.com>
2020-07-03 08:28:21 -0500

    Start server express

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("This begins");
+const express = require("express");
+const app = express();
+
+const { PORT, HELP_URL } = process.env;
+const { log } = console;
+
+app.get("/help", (_req, res) => {
+  res.writeHeader(200, { "Content-Type": "text/html" });
+  res.write(`Visit <a href="${HELP_URL}">this</a> to more info!`);
+  res.end();
+});
+
+app.listen(PORT, () => {
+  log(`Server on http://localhost:${PORT}`);
+});
